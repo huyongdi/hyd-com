43b7cbc822b68cc84260049487a43362fa9ba838
354224080@qq.com <huyongdi525>
2020-12-03 16:36:54 +0800

    增加APP声明

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router"
 import Home from "../views/Home.vue"
 import About from "../views/About.vue"
 import Article from "../views/Article.vue"
+import APP from "../views/APP.vue"
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/article",
     name: "文章",
     component: Article
+  },
+  {
+    path: "/app",
+    name: "APP声明",
+    component: APP
   }
 ]
 
---
+++ src/views/APP.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    本APP不收集任何手机信息, 只需要实时获取地理位置进行天气显示
+    本APP无任何形式收费
+    用户信息保存在阿里云Dcloud
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  mounted() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less">
+.home {
+
+}
+</style>
